85ead665be2a441e38868076a38a4dcc7a6330cb
MyshaZeshan <mysha.zeshan01@gmail.com>
2026-07-21 00:15:29 +0500

    created pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import SignIn from './Pages/SignIn'
+import SignOut from './Pages/SignOut'
+import Profile from './Pages/Profile'
+import About from './Pages/About'
 
 const App = () => {
   return (
-    <section className="text-red-500">App</section>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/SignIn" element={<SignIn/>}/>
+        <Route path="/SignOut" element={<SignOut/>}/>
+        <Route path="/Profile" element={<Profile/>}/>
+        <Route path="/About" element={<About/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Pages/Profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <section className="text-blue-500">Profile</section>
+  )
+}
+
+export default Profile
