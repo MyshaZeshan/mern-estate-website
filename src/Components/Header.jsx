efc3c5e877cd9a7b41abe4872e96ba9fd3da0fb9
MyshaZeshan <mysha.zeshan01@gmail.com>
2026-07-22 00:40:55 +0500

    Added the header nav

---
+++ src/Components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {FaSearch} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header className="bg-blue-200 shadow-mist-400 p-4">
+        <div className="flex items-center justify-between max-w-6xl mx-auto">
+            <Link to="/">
+                <div className=" text-sm ">
+                    <span className=" font-bold sm:text-2xl text-yellow-600">MZ</span>
+                    <span>Estate</span>
+                </div>
+            </Link>
+            <form className="border border-slate-600 p-2 rounded-lg flex items-center">
+                <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64"/>
+                <FaSearch className = "text-slate-600"/>
+            </form>
+            <ul className = "flex gap-4">
+                <Link to="/">
+                    <li className='hidden sm:inline text-blue-900 font-bold hover:underline hover:decoration-yellow-600 hover:decoration-2 hover:underline-offset-4 transition-all duration-300'>Home</li>
+                </Link>
+                <Link to="/About">
+                    <li className='hidden sm:inline text-blue-900 font-bold hover:underline hover:decoration-yellow-600 hover:decoration-2 hover:underline-offset-4 transition-all duration-300'>About</li>
+                </Link>
+                <Link to="/SignIn">
+                    <li className='text-blue-900 font-bold hover:underline hover:decoration-yellow-600 hover:decoration-2 hover:underline-offset-4 transition-all duration-300'>SignIn</li>
+                </Link>
+            </ul>
+        </div>
+    </header>
+  )
+}
+
+export default Header
