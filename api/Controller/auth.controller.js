cd6622e5ae1925469edd5d1169386b6668b72b5d
MyshaZeshan <mysha.zeshan01@gmail.com>
2026-07-30 19:08:11 +0500

    Created signin api route

--- api/Controller/auth.controller.js
+++ api/Controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../Models/User.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 //Location / Title: It lives in autgController.js, so it’s a Controller function.
 //Role / Job: It handles the incoming request for that specific route, so it’s the Route Handler.
@@ -12,9 +13,25 @@ export const signup = async (req,res,next)=>{
     try{
         await newUser.save();
         res.status(201).json({success: true , msg: 'user created successfully'});
+
     }
     catch(error){
         next(error);   
     }
     
+}
+
+export const signin = async(req,res,next)=>{
+    const {email,password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404,'User not found'));
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword) return next(errorHandler(401,'Wrong Credentials!'));
+        const token = jwt.sign({id : validUser._id},process.env.JWT_SECRET)
+        const {password: pass, ...rest}= validUser._doc;
+        res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
 }
--- api/Routes/auth.route.js
+++ api/Routes/auth.route.js
@@ -1,7 +1,9 @@
 import express from "express";
-import {signup} from "../Controller/auth.controller.js";
+import {signup,signin} from "../Controller/auth.controller.js";
+
 const router  =express.Router();
 
 router.post("/signup",signup);
+router.post("/signin",signin);
 
 export default router;
