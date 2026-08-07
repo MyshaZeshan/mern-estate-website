7e18e4b89ae60fe647392663ac38c12c7d12697a
MyshaZeshan <mysha.zeshan01@gmail.com>
2026-08-07 22:03:51 +0500

    created userUpdate API

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./Routes/user.route.js";
 import authRoutes from "./Routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Error connecting to MongoDB", err);
 })
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.listen(3000,()=>{
     console.log("Server is running on port 3000!!!!!");
@@ -18,7 +20,6 @@ app.listen(3000,()=>{
 
 
 app.use("/api/user",userRoutes)
-
 app.use("/api/auth",authRoutes)
 
 app.use((err,req,res,next)=>{
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useRef } from 'react'
 
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user)
+  const fileRef = useRef(null);
   return (
     <section  className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl font-extrabold text-center my-7 rgb(34,34,34)'>Profile</h1>
+      <h1 className='text-3xl font-extrabold text-center my-7 text-[rgb(34,34,34)]'>Profile</h1>
       <form className='flex flex-col items-center gap-4 bg-[rgb(250,243,225)] p-8 rounded-lg shadow-md max-w-md mx-auto'>
-        <img src={currentUser.photo} alt='Profile' className='w-32 h-32 rounded-full object-cover mx-auto cursor-pointer' />
-        <input type='text' value={currentUser.username} readOnly className='border border-gray-300 p-2 rounded-lg w-full text-center' id='username' />
-        <input type='email' value={currentUser.email} readOnly className='border border-gray-300 p-2 rounded-lg w-full text-center' id='email' />
+
+        <input type='file' ref={fileRef} className='hidden' accept='image/*'/>
+        <img onClick = {()=>fileRef.current.click()} src={currentUser.photo} alt='Profile' className='w-32 h-32 rounded-full object-cover mx-auto cursor-pointer' />
+        <input type='text' placeholder='username' id='username' className='border border-gray-300 p-2 rounded-lg w-full text-center' />
+        <input type='email' placeholder='email' className='border border-gray-300 p-2 rounded-lg w-full text-center' id='email' />
         <input type='password' placeholder='password' id = 'password' className='border border-gray-300 p-2 rounded-lg w-full text-center' />
 
         <button className='text-[rgb(250,243,225)] bg-[rgb(255,109,31)] font-extrabold p-3 border rounded-lg w-full hover:opacity-95 disabled:opacity-80'>UPDATE</button>
