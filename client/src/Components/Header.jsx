88bc3b7e67479e2ff0642f9ce26309081ee3d249
MyshaZeshan <mysha.zeshan01@gmail.com>
2026-08-07 15:26:46 +0500

    profile UI done

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -7,29 +7,29 @@ const Header = () => {
   const { currentUser } = useSelector((state) => state.user)
 
   return (
-    <header className="bg-blue-200 shadow-mist-400 p-4">
+    <header className="bg-orange-500 shadow-mist-400 p-4">
         <div className="flex items-center justify-between max-w-6xl mx-auto">
             <Link to="/">
                 <div className=" text-sm ">
-                    <span className=" font-bold sm:text-2xl text-yellow-600">MZ</span>
-                    <span>Estate</span>
+                    <span className=" font-bold sm:text-2xl text-[rgb(245,231,198)]">MZ</span>
+                    <span className="text-[rgb(34,34,34)]">Estate</span>
                 </div>
             </Link>
-            <form className="border border-slate-600 p-2 rounded-lg flex items-center">
+            <form className="border border-slate-600 p-2 rounded-lg flex items-center bg-[rgb(250,243,225)]">
                 <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64"/>
                 <FaSearch className = "text-slate-600"/>
             </form>
             <ul className = "flex gap-4">
                 <Link to="/">
-                    <li className='hidden sm:inline text-blue-900  hover:underline hover:decoration-yellow-600 hover:decoration-2 hover:underline-offset-4 transition-all duration-300'>Home</li>
+                    <li className='hidden sm:inline text-[rgb(245,231,198)] hover:underline hover:decoration-black hover:decoration-2 hover:underline-offset-4 transition-all duration-300'>Home</li>
                 </Link>
                 <Link to="/About">
-                    <li className='hidden sm:inline text-blue-900  hover:underline hover:decoration-yellow-600 hover:decoration-2 hover:underline-offset-4 transition-all duration-300'>About</li>
+                    <li className='hidden sm:inline text-[rgb(245,231,198)]  hover:underline hover:decoration-black hover:decoration-2 hover:underline-offset-4 transition-all duration-300'>About</li>
                 </Link>
                 <Link to='/Profile'>
                     {currentUser ? (
                         <img src={currentUser.photo } alt='profile-pic' className="w-7 h-7 rounded-full object-cover"/>) : (
-                        <li className='text-blue-900  hover:underline hover:decoration-yellow-600 hover:decoration-2 hover:underline-offset-4 transition-all duration-300'>SignIn</li>
+                        <li className='text-[rgb(245,231,198)] hover:underline hover:decoration-black hover:decoration-2 hover:underline-offset-4 transition-all duration-300'>SignIn</li>
                     )}
                 </Link>
 
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+  const { currentUser } = useSelector((state) => state.user)
   return (
-    <section className="text-blue-500">Profile</section>
+    <section  className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-extrabold text-center my-7 rgb(34,34,34)'>Profile</h1>
+      <form className='flex flex-col items-center gap-4 bg-[rgb(250,243,225)] p-8 rounded-lg shadow-md max-w-md mx-auto'>
+        <img src={currentUser.photo} alt='Profile' className='w-32 h-32 rounded-full object-cover mx-auto cursor-pointer' />
+        <input type='text' value={currentUser.username} readOnly className='border border-gray-300 p-2 rounded-lg w-full text-center' id='username' />
+        <input type='email' value={currentUser.email} readOnly className='border border-gray-300 p-2 rounded-lg w-full text-center' id='email' />
+        <input type='password' placeholder='password' id = 'password' className='border border-gray-300 p-2 rounded-lg w-full text-center' />
+
+        <button className='text-[rgb(250,243,225)] bg-[rgb(255,109,31)] font-extrabold p-3 border rounded-lg w-full hover:opacity-95 disabled:opacity-80'>UPDATE</button>
+      </form>
+      <div className= "flex justify-between mt-5">
+        <span className='text-red-500 cursor-pointer hover:underline '>Delete Account</span>
+        <span className='text-red-500 cursor-pointer hover:underline'>Sign Out</span>
+      </div>
+    </section>
   )
 }
 
