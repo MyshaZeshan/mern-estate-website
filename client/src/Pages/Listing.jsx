8dcde4b5329fee34f7fe7a34bc43338145905aa1
MyshaZeshan <mysha.zeshan01@gmail.com>
2026-08-22 00:11:34 +0500

    create listng page UI

--- client/src/Pages/Listing.jsx
+++ client/src/Pages/Listing.jsx
@@ -54,6 +54,55 @@ const Listing = () => {
                 ))}
             </Swiper>
             </div>}
+            <div className='bg-[rgb(250,243,225)]'>
+                <div className="max-w-5xl mx-auto px-6 py-8 ">
+
+                    <h1 className="text-3xl font-bold">{listing.name}</h1>
+                    <p className="text-gray-700 mt-1">{listing.address}</p>
+
+                    <p className="mt-4 text-gray-700">{listing.description}</p>
+
+                    <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-6">
+                        <div className="border border-[rgb(255,109,31)] p-4 rounded-lg">
+                            🛏️ {listing.bedrooms} Bedrooms
+                        </div>
+
+                        <div className="border border-[rgb(255,109,31)] p-4 rounded-lg">
+                            🛁 {listing.bathrooms} Bathrooms
+                        </div>
+
+                        <div className="border border-[rgb(255,109,31)] p-4 rounded-lg">
+                            🚗 {listing.parking ? "Parking" : "No Parking"}
+                        </div>
+
+                        <div className="border border-[rgb(255,109,31)] p-4 rounded-lg">
+                            🛋️ {listing.furnished ? "Furnished" : "Unfurnished"}
+                        </div>
+                    </div>
+
+                    <div className="mt-6">
+                        {listing.offer && listing.discountPrice > 0 ? (
+                            <>
+                                <span className="text-3xl font-bold text-[rgb(255,109,31)]">
+                                    ${listing.discountPrice}
+                                </span>
+                                <span className="ml-3 line-through text-gray-500">
+                                    ${listing.regularPrice}
+                                </span>
+                            </>
+                        ) : (
+                            <span className="text-3xl font-bold text-[rgb(255,109,31)]">
+                                ${listing.regularPrice}
+                            </span>
+                        )}
+
+                        <span className="ml-4 uppercase font-semibold text-[rgb(250,243,225)] bg-[rgb(255,109,31)] px-3 py-1 rounded">
+                            For {listing.type}
+                        </span>
+                    </div>
+
+                </div>
+            </div>
     </main>
   )
 }
