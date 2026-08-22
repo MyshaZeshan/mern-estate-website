28bc5c8d2797357e1c871182146b459c7b775551
MyshaZeshan <mysha.zeshan01@gmail.com>
2026-08-23 00:57:12 +0500

    created seacch API route

--- api/Controller/listing.controller.js
+++ api/Controller/listing.controller.js
@@ -57,3 +57,41 @@ export const getListing = async(req,res,next) =>{
         next(error);
     }
 }
+
+export const getListingforSearch = async (req,res,next) =>{
+    try {
+        const limit = parseInt(req.query.list) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        let offer = req.query.offer;
+        if(offer === 'undefined' || offer==='false'){
+            offer = {$in:[false,true]} //$in means find all values inside the list
+        }
+        let furnished = req.query.furnished;
+        if(furnished === 'undefined' || furnished==='false'){
+            furnished = {$in:[false,true]} //$in means find all values inside the list
+        }
+        let parking = req.query.parking;
+        if(parking === 'undefined' || parking==='false'){
+            parking = {$in:[false,true]} //$in means find all values inside the list
+        }
+        let type = req.query.type;
+        if(type === 'undefined' || type==='all'){
+            type = {$in:['sale','rent']} //$in means find all values inside the list
+        }
+        const searchTerm = req.query.searchTerm || ''
+        const sort = req.query.sort || 'createdAt';
+        const order = req.query.order || 'desc';
+
+        const listings = await Listing.find({
+            name : {$regex: searchTerm, $options:'i'}, //regex can find any word letter and option means uppercase or lowercase
+            offer,
+            furnished,
+            parking,
+            type,
+        }).sort({[sort]:order}).limit(limit).skip(startIndex);
+
+        return res.status(200).json(listings);
+    } catch (error) {
+        next(error);
+    }
+}
--- api/Routes/listing.route.js
+++ api/Routes/listing.route.js
@@ -3,6 +3,7 @@ import { createListing, getListing } from '../Controller/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 import { deletelisting } from '../Controller/listing.controller.js';
 import { updatelisting } from '../Controller/listing.controller.js';
+import { getListingforSearch } from '../Controller/listing.controller.js';
 
 
 
@@ -12,4 +13,5 @@ router.post('/create',verifyToken,createListing);
 router.delete('/delete/:id',verifyToken,deletelisting);
 router.post('/update/:id',verifyToken,updatelisting);
 router.get('/get/:id',getListing);
+router.get('/get',getListingforSearch);
 export default router;
