7b64f8128afbca58a01dab83bb05bd9571d0a1fe
MyshaZeshan <mysha.zeshan01@gmail.com>
2026-08-29 01:48:59 +0500

    About page completed

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='mx-60 my-20 bg-[rgb(250,243,225)] p-10 border rounded-lg'>
+      <h1 className='md:text-5xl font-bold  '>About Us</h1>
+      <p className='mt-10 leading-relaxed'>
+        MZ Estate is your one-stop platform for finding the perfect place to live. Whether you're looking to buy your dream home, rent a cozy apartment, or explore properties currently on offer, we bring together a wide range of listings across top locations all in one simple, easy-to-use experience.
+        <br/>
+        Our mission is to make property hunting stress-free. With detailed listings, high-quality images, and clear information on pricing, amenities, and location, MZ Estate helps you make confident decisions whether you're a first-time renter or a seasoned buyer.
+
+        We believe finding a home shouldn't be complicated. That's why MZ Estate focuses on speed, simplicity, and transparency giving you the tools to search, compare, and connect with listings that actually fit your needs and budget.
+        <br/>
+        From spacious family houses to modern downtown apartments, MZ Estate is built to help you find not just a property, but a place to call home.
+      </p>
+    </div>
   )
 }
 
